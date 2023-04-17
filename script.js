f75590925d99ac909363d145cf79578f7f8c2639
Freddy193 <fredericst@viken.no>
2023-04-17 14:51:20 +0200

    hei

---
+++ script.js
@@ -0,0 +1,29 @@
+let data_array = [];
+let grupper_array = ["25-34","35-44","45-54","55-64","65-74","75-79"];
+const csvURL = "sosialeMedier.csv";
+let CanvasEl = document.getElementById("canvas");
+let ctx = CanvasEl.getContext("2d");
+
+fetch(csvURL)
+    .then(response => response.text())  //henter ut teksten fra csv filen
+    .then(csvData => {  // Vi sier at vi har en csv fil som skal gjøre denne funksjonen
+        Papa.parse(csvData, {
+            delimiter: ";",
+            header: true,
+            dynamicTyping: true,
+            complete: function(results){
+                console.log(results.data);
+                data_array = results.data;
+            },
+            error:function(error){
+                console.log(error);
+            },
+        });
+    })
+    ctx.moveTo(0,0);
+    function lagCanvas(array){
+        for (let i = 0; i < array.length; i++) {
+            ctx.lineTo(i,array[i]); 
+            ctx.stroke();
+        } 
+    }
