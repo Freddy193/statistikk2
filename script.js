3297f3f5fedbde7a3c5bfbad553a0d612189a966
slukofanten <slukofanten@gmail.com>
2023-04-19 10:11:51 +0200

    hei

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let data_array = [];
-let grupper_array = ["25-34","35-44","45-54","55-64","65-74","75-79"];
+let grupper_array = ["25-34", "35-44", "45-54", "55-64", "65-74", "75-79"];
 const csvURL = "sosialeMedier.csv";
 let CanvasEl = document.getElementById("canvas");
 let ctx = CanvasEl.getContext("2d");
@@ -11,19 +11,19 @@ fetch(csvURL)
             delimiter: ";",
             header: true,
             dynamicTyping: true,
-            complete: function(results){
+            complete: function (results) {
                 console.log(results.data);
                 data_array = results.data;
             },
-            error:function(error){
+            error: function (error) {
                 console.log(error);
             },
         });
     })
-    ctx.moveTo(0,0);
-    function lagCanvas(array){
-        for (let i = 2010; i < array.length; i++) {
-            ctx.lineTo(i,array[i]); 
-            ctx.stroke();
-        } 
+ctx.moveTo(0, 0);
+function lagCanvas(array) {
+    for (let i = 2010; i < array.length; i++) {
+        ctx.lineTo(i, array[i]);
+        ctx.stroke();
     }
+}
