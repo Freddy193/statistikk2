913a5cc8b736511d9ef4911e8d760e906d3e8ba7
Birkemann69 <127754626+Birkemann69@users.noreply.github.com>
2023-04-19 10:01:57 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,69 @@
+let data_array = [];
+let grupper_array = ["25-34","35-44","45-54","55-64","65-74","75-79"];
+const csvURL = "sosialeMedier.csv";
+let CanvasEl = document.getElementById("canvas");
+let ctx = CanvasEl.getContext("2d");
+
+fetch(csvURL)
+    .then(response => response.text())
+    .then(csvData => {
+        Papa.parse(csvData, {
+            delimiter: ";",
+            header: true,
+            dynamicTyping: true,
+            complete: function(results){
+                console.log(results.data);
+                data_array = results.data;
+                lagCanvas(data_array);
+            },
+            error:function(error){
+                console.log(error);
+            },
+        });
+    })
+
+function getMaxValue(array) {
+    let maxValue = 0;
+    for (let i = 0; i < array.length; i++) {
+        for (const key in array[i]) {
+            if (array[i][key] > maxValue) {
+                maxValue = array[i][key];
+            }
+        }
+    }
+    return maxValue;
+}
+
+function drawAxes() {
+    ctx.beginPath();
+    ctx.moveTo(50, 0);
+    ctx.lineTo(50, CanvasEl.height - 50);
+    ctx.lineTo(CanvasEl.width, CanvasEl.height - 50);
+    ctx.stroke();
+
+    for (let i = 2010, xPos = 60; i <= 2010 + data_array.length - 1; i++, xPos += 50) {
+        ctx.fillText(i, xPos, CanvasEl.height - 35);
+    }
+
+    for (let i = 0, yPos = CanvasEl.height - 60; i <= 100; i += 10, yPos -= 50) {
+        ctx.fillText(i + "%", 20, yPos);
+    }
+}
+
+function lagCanvas(array){
+    let maxValue = getMaxValue(array);
+    drawAxes();
+
+    ctx.beginPath();
+    for (let i = 0; i < array.length; i++) {
+        let year = 2010 + i;
+        let x = 60 + i * 50;
+        let y = (CanvasEl.height - 50) - (array[i][year] / maxValue) * (CanvasEl.height - 100);
+        if (i === 0) {
+            ctx.moveTo(x, y);
+        } else {
+            ctx.lineTo(x, y);
+        }
+        ctx.stroke();
+    }
+}
